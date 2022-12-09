2e2da9034924500407ef2baabc6d41039a609f9b
stanislavi18061989 <96617178+stanislavbukaevsky@users.noreply.github.com>
2022-12-09 15:07:58 +0300

    Первый коммит

---
+++ src/main/java/ru/hogwarts/schoolfive/SchoolFiveApplication.java
@@ -0,0 +1,13 @@
+package ru.hogwarts.schoolfive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolFiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolFiveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/hogwarts/schoolfive/SchoolFiveApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.hogwarts.schoolfive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolFiveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
